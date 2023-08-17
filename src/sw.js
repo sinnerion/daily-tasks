4b110eac6e3623f65ac8c1a84e3f4f19dc0bb341
sinnerion <bobrelius@gmail.com>
2023-08-17 14:30:28 +0300

    v1.0.0: service worker sw.js (simple version) is done

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 's-app-v1';
+const staticCacheName = 's-app-v0';
 const assetsUrls = [
     'index.html',
     '/js/app.js',
@@ -10,10 +10,22 @@ self.addEventListener('install', async event => {
   await cache.addAll(assetsUrls);
 });
 
-self.addEventListener('activate', event => {
-  console.log('[SW]: activate');
+self.addEventListener('activate', async event => {
+  const cacheNames = await caches.keys();
+  await Promise.all(
+      cacheNames
+          .filter(name => name !== staticCacheName)
+          .map(name => caches.delete(name))
+  );
 });
 
 self.addEventListener('fetch', event => {
-  console.log('Fetch', event.request.url);
+  // console.log('Fetch', event.request.url);
+
+  event.respondWith(cacheFirst(event.request));
 });
+
+async  function cacheFirst(request) {
+  const cached = await caches.match(request);
+  return cached ?? await fetch(request);
+}
